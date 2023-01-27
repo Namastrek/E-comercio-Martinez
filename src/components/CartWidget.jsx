a81ec141c8ad66e52861fd4c3e44be17bfb39ddf
Nahuel Martinez <nahuelmartt25@gmail.com>
2023-01-27 14:55:19 -0300

    card

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,5 +1,3 @@
-import "bootstrap-icons/font/bootstrap-icons.css"
-
 const CartWidget = () => {
     return(
         <i class="bi bi-cart3">0</i>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,3 @@
-import "bootstrap-icons/font/bootstrap-icons.css"
 import CartWidget from './CartWidget'
 
 const NavBar = () => {
